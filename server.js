5bfd7ca5447b95ad6095025fe88901f9a4b54817
root <root@Lenovo>
2024-11-12 14:44:14 +0530

    Updated server.js script.js added multiple files

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html at the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html','fuzz_target.c','inputs','outputs'));
 });
 
 // Endpoint to receive the command and execute it
