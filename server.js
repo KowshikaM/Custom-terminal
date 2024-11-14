4556c594f27dac7234884ed059476c103bf93383
root <root@Lenovo>
2024-11-14 21:34:49 +0530

    Update server.js origin*

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 const cors = require('cors');
 
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({origin:'*'}));
